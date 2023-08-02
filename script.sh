afabe2b627173e1068b242e1d94dc754dda48574
Mike <mikemoore88@gmail.com>
2023-08-02 22:58:45 +0100

    Add comments

--- script.sh
+++ script.sh
@@ -4,23 +4,30 @@ DIRECTORY=/Users/username/BAO/Code/projectname
 IMAGE_CDN_URL=https://store-name.myshopify.com/cdn/shop/files/
 #VIDEO_CDN_URL=https://cdn.shopify.com/videos/c/vp/
 
+# Get json files which could reference images
 FILES=$(find $DIRECTORY -maxdepth 2 -mindepth 2 -iname "*.json" -not -path "*/locales/*" -not -path "*/assets/*" -type f)
 
+# Declare an array to store URLS
 declare -a URLS
 
+# Loop through the found files
 for FILE in $FILES
 do
+  # Store the lines of the files in an array
   LINES=`cat $FILE`
 
+  # Loop through the lines of the files
   for LINE in $LINES
   do
 
+    # If the line contains "shopify:\/\/shop_images" build a URL and append it to the URLS array
     if [[ "$LINE" == *"shopify:\/\/shop_images"* ]]; then
       LINE=${LINE//"shopify:\/\/shop_images\/"/}
       LINE=${LINE//'"'/}
       LINE=${LINE//','/}
       LINE="$IMAGE_CDN_URL$LINE"
 
+      # Only append the URL to the array when it's not already in the array
       if [[ ! ${URLS[@]} =~ $LINE ]]
       then
         URLS=("${URLS[@]}" $LINE)
@@ -37,8 +44,10 @@ do
   done
 done
 
+# Make a downloads directory if one doesn't already exist
 mkdir -p ./downloads
 
+# Loop through the URLS array and downloas each image using wget
 for URL in ${URLS[@]}
 do
   wget -P "./downloads" "$URL"
